7ee15cf120b18ac513ccc5ebdf76188df2e85c86
DXLeGenD <arfathasif900@gmail.com>
2023-12-31 12:38:02 +0530

    projects updated

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ function goToContact() {
     a.href = "#contact-loc"
     a.click()
 }
+
+
+const lostAndFound = document.getElementById('lostAndFoundHub')
+lostAndFound.addEventListener("click", lostAndFoundHub)
+
+function lostAndFoundHub() {
+    const a = document.createElement('a')
+    a.href = "https://lost-and-found-hub.netlify.app/"
+    a.target = "_blank"
+    a.click()
+}
+
+
+const todoLists = document.getElementById('todoList')
+todoLists.addEventListener("click", todoList)
+
+function todoList() {
+    const a = document.createElement('a')
+    a.href = "https://todos-list-by-me.netlify.app/"
+    a.target = "_blank"
+    a.click()
+}
+
+
+
+const greenGroves = document.getElementById('greenGrove')
+greenGroves.addEventListener("click", greenGrove)
+
+function greenGrove() {
+    const a = document.createElement('a')
+    a.href = "https://green-grove.netlify.app/"
+    a.target = "_blank"
+    a.click()
+}
